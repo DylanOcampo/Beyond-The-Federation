c43968804919f2b333ae441dbd62063542d9e33b
AngelloxSP <angelloxsp@gmail.com>
2023-09-19 00:50:29 -0600

    Minigame01
    Minijuego base y otras cosillas

---
+++ Assets/Scripts/Minijuegos/Minijuego01/ManagerMG01.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerMG01 : MonoBehaviour
+{
+    public GameObject minijuego01;
+    public GameObject limpiaflechas;
+
+
+    private void Start()
+    {
+        desactivarMinijuego();
+    }
+
+    public void activarMinijuego()
+    {
+        minijuego01.SetActive(true);
+        limpiaflechas.SetActive(false);
+    }
+    
+    public void desactivarMinijuego()
+    {
+        minijuego01.SetActive(false);
+        limpiaflechas.SetActive(true);
+    }
+
+}
